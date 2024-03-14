da8614d3418f404d9f2fefa26b4b67022cbe4af2
GuillermoRomoVera <you@example.com>
2024-03-14 09:49:12 -0500

    cambiosmarzo

---
+++ Unidad 2/JsDoc Opciones/src/modulo.js
@@ -0,0 +1,11 @@
+/**
+ * Calcular la operación de 3 variables
+ * @param {Number} a Variable Tipo int
+ * @param {Number} b Variable Tipo int
+ * @param {Number} Exponente Variable Tipo int de Exponente en la Multiplicación
+ * @returns Number
+ */
+
+function multiplicar(a, b, Exponente){
+    return(a * b * Exponente);
+}
---
+++ Unidad 2/ServidorErrores copy/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express()
+const ruta_mestros = require('.')
+
+app.get('/maestros', (req, res,next) => {
+  if(tru){
+    res.send("respondiendo maestros");
+  }
+  else{
+    res.send("respondiendo");
+  }
+  })
+  
+
+app.get('/alumnos', (req, res,next) => {
+try {
+throw new Error("ocurrio una tragedia")
+}
+catch(e){
+  next(e)
+  }
+  // res.send('Respondiendo servidor')
+})
+
+
+app.listen(3000, () => {
+  console.log(`Example app listening on port 3000`)
+})
+// app.use( (req, res,next) => {
+//   res.status(404).send('Recursos no encontrado')
+// })
+app.use((err, req, res, next) => {
+  res.status(500).send( err.message);
+});
