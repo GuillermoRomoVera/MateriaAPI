2ecf5fe4080eaf96e8c4c6cea3993c3ff1242dc0
GuillermoRomoVera <you@example.com>
2024-03-18 12:29:29 -0500

    cambiosenvio

---
+++ Unidad 2/EnvioArchivos/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const path = require("path")
+const app = express()
+
+
+app.get('/download', (req, res, next) => {
+  res.download(path.join(__dirname, '/imagenes/tec.png'));
+})
+
+app.get('/send', (req, res, next) => {
+  res.sendFile(path.join(__dirname, '/imagenes/tec.png'));
+})
+
+app.listen(3000, () => {
+  console.log('Example app listening on port 3000');
+});
